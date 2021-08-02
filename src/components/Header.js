0906ccae97519c705d9f82f46330ff5d546910aa
katherine-rock <katherine_rock@hotmail.com>
2021-08-02 12:32:52 +1000

    Add header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import DevLinks_header_image from '../images/DevLinks_header_image.png'
+
 const Header = () => {
     return (
       <div >
             <h1>Dev Links</h1>
+            <img src={DevLinks_header_image} alt="Descriptive text"/>
             <p>Helpful links for budding software developers</p>
       </div>
     )
