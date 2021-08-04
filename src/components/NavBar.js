6f027a429aa22050b13b2b58037c8ee2d62e57f2
katherine-rock <katherine_rock@hotmail.com>
2021-08-04 17:31:08 +1000

    Format NavBar for moblie

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,25 +7,25 @@ import {
 
 const NavBar = () => {
     return (
-        <div flex className="flex justify-center space-x-4 p-4">
-            <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow text-xl p-4 w-32">
-            <Link to="/">Home</Link>
+        <div flex className="flex justify-center space-x-4 p-4 sm:flex-row md: flex-col justify-center gap-2">
+            <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow text-xl p-4 w-48 hover:underline mx-4 sm:mx-0">
+            <Link to="/">&#60; Home / &#62;</Link>
             </button>
 
-            <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow text-xl p-4 w-32">
-            <Link to="/ruby">Ruby</Link>
+            <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow text-xl p-4 w-48 hover:underline">
+            <Link to="/ruby">&#60; Ruby / &#62;</Link>
             </button>
 
-            <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow text-xl p-4 w-32">
-            <Link to="/rails">Rails</Link>
+            <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow text-xl p-4 w-48 hover:underline">
+            <Link to="/rails">&#60; Rails / &#62;</Link>
             </button>
 
-            <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow text-xl p-4 w-32">
-            <Link to="/javascript">JavaScript</Link>
+            <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow text-xl p-4 w-48 hover:underline">
+            <Link to="/javascript">&#60; JavaScript / &#62;</Link>
             </button>
 
-            <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow text-xl p-4 w-32">
-            <Link to="/react">React</Link>
+            <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow text-xl p-4 w-48 hover:underline">
+            <Link to="/react">&#60; React / &#62;</Link>
             </button>
         </div>
     )
