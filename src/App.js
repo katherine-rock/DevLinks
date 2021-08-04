6a172c06c77d810e6b8b9784824594b416c3b189
katherine-rock <katherine_rock@hotmail.com>
2021-08-04 16:40:06 +1000

    Add React router DOM to give each language or framework a dedicated page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,65 @@ import Rails from './components/Rails'
 import Javascript from './components/Javascript'
 import ReactLinks from './components/React'
 import Footer from './components/Footer'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <Ruby />
-      <Rails />
-      <Javascript />
-      <ReactLinks />
-      <Footer />
-    </>
+      <div>
+        <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow p-4">
+          <Link to="/">Home</Link>
+        </button>
+
+        <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow p-4">
+          <Link to="/ruby">Ruby</Link>
+        </button>
+
+        <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow p-4">
+          <Link to="/rails">Rails</Link>
+        </button>
+
+        <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow p-4">
+          <Link to="/javascript">JavaScript</Link>
+        </button>
+
+        <button className="border-4 rounded-md border-devlightblue bg-devdarkblue text-devyellow p-4">
+          <Link to="/react">React</Link>
+        </button>
+
+        <Switch>
+          <Route path="/ruby">
+            <Ruby />
+          </Route>
+
+          <Route path="/rails">
+            <Rails />
+          </Route>
+
+          <Route path="/javascript">
+            <Javascript />
+          </Route>
+
+          <Route path="/react">
+            <ReactLinks />
+          </Route>
+
+          <Route path="/">
+            <Ruby />
+            <Rails />
+            <Javascript />
+            <ReactLinks />
+          </Route>
+        </Switch>
+      </div>
+      <Footer/>
+    </Router>
   )
 }
 
