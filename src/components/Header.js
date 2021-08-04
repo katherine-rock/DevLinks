90701473b0eee69b086977564922a5bfc986d58a
katherine-rock <katherine_rock@hotmail.com>
2021-08-04 12:28:36 +1000

    Add basic header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,15 @@ import DevLinks_header_image from '../images/DevLinks_header_image.png'
 
 const Header = () => {
     return (
-      <div >
-        <h1>Dev Links</h1>
-        <img src={DevLinks_header_image} alt="Neon computer screen with html tags" height="200px" />
-        <p>Helpful links for budding software developers</p>
+      <div className="w-screen bg-black flex flex-col justify-center">
+        <div className="flex flex-row items-center justify-center">
+          <h1 className="text-blue-400 text-7xl">Dev Links</h1>
+          <img className="max-h-52" 
+          src={DevLinks_header_image} 
+          alt="Neon computer screen with html tags" />
+        </div>
+
+        <p className="text-white text-xl flex flex-row justify-center pr-6 pb-6">Helpful links for budding software developers</p>
       </div>
     )
   }
