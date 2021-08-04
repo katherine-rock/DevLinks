950c56c5932392e0d2aa80560f56648e978759db
katherine-rock <katherine_rock@hotmail.com>
2021-08-04 16:00:10 +1000

    Set column width for responsive design, wrap long descriptions and urls

--- src/components/Ruby.js
+++ src/components/Ruby.js
@@ -11,13 +11,13 @@ const Ruby = () => {
     return (
       <div className="w-screen flex flex-col">
         <h2 className="text-3xl text-center pt-8">Ruby</h2>
-        <div className="flex flex-row p-8 max-w-max space-x-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="flex flex-row p-8 max-w-max grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {ruby_array.map(item =>
             <div className="border-4 rounded-lg border-devalmostblack xp-4" key={item.id}>
               <h3 className="text-2xl text-center bg-devalmostblack text-devyellow p-2">{item.title}</h3>
               <img src={item.screenshot} alt={item.title} width="100%"></img>
-              <p className="pt-3 pl-8">{item.description}</p>
-              <a className="underline pb-4 pl-8" href={item.url} target="_blank">{item.url}</a>
+              <p className="pt-3 pl-8 pr-8">{item.description}</p>
+              <a className="underline pb-4 pl-8 pr-8 flex flex-wrap break-all" href={item.url} target="_blank">{item.url}</a>
               <br/>
 
             </div> 
