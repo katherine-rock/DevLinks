2b9201bee55ca2b1dee38aaac3a9e5f5a0911a85
katherine-rock <katherine_rock@hotmail.com>
2021-08-08 12:27:03 +1000

    Add test of home page links in Header

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -19,21 +19,37 @@ afterEach(() => {
 });
 
 describe('Header', () => {
-    it('should show the app name', () => {
-        render(<Router><Header /></Router>);
-        const text = screen.getByText(/Dev Links/i);
-        expect(text).toBeInTheDocument();
-    })
+  it('should show the app name', () => {
+      render(<Router><Header /></Router>);
+      const text = screen.getByText(/Dev Links/i);
+      expect(text).toBeInTheDocument();
+  })
 
-    it('should show the app description', () => {
-        render(<Router><Header /></Router>);
-        const text = screen.getByText(/Helpful links for budding software developers/i);
-        expect(text).toBeInTheDocument();
-    })
+  it('should show the app description', () => {
+      render(<Router><Header /></Router>);
+      const text = screen.getByText(/Helpful links for budding software developers/i);
+      expect(text).toBeInTheDocument();
+  })
 
-    it('should show the header icon', () => {
-        render(<Router><Header /></Router>);
-        const icon = screen.getByAltText(/Neon computer screen with html tags/i);
-        expect(icon).toBeInTheDocument();
-    })
+  it('should show the header icon', () => {
+      render(<Router><Header /></Router>);
+      const icon = screen.getByAltText(/Neon computer screen with html tags/i);
+      expect(icon).toBeInTheDocument();
+  })
+
+  it('header text Dev Links should link to home page', () => {
+    render(
+        <Router><Header/></Router>
+    );
+    const linkElement = screen.getByText(/Dev Links/);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('should link the header icon to the home page', () => {
+    render(
+      <Router><Header/></Router>
+    );
+    const linkElement = screen.getByAltText(/Neon computer screen with html tags/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
